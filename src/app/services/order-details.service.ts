a826195bd61338be076e4b9402676e4dd4fef6cc
VaibhavPachpute21 <vpachpute21@gmail.com>
2022-03-13 19:24:42 +0530

    modified home

--- src/app/services/order-details.service.ts
+++ src/app/services/order-details.service.ts
@@ -10,7 +10,7 @@ export class OrderDetailsService {
   foodDetails=[
     {id:1, image:"../../../assets/burger.jpg" ,name:"Buger",price:"$5",details:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"},
     {id:2, image:"../../../assets/cake.jpg" ,name:"Cake",price:"$5",details:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"},
-    {id:3, image:"../../../assets/food.jpg" ,name:"Food Item3",price:"$5",details:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"},
+    {id:3, image:"../../../assets/food.jpg" ,name:"blah blah",price:"$5",details:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"},
     {id:4, image:"../../../assets/pancake.jpg" ,name:"Pancake",price:"$5",details:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"},
     
   ]
